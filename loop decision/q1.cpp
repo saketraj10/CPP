4b884dd7b1ab3042cb5ad0f250385a93de12dc9f
saketraj10 <110519922+saketraj10@users.noreply.github.com>
2024-02-03 14:17:46 +0530

    Add files via upload

---
+++ loop decision/q1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=10, b=5;
+    if(x>5){
+        if(b>5)
+        cout<<"a and b are > 5"<<endl;
+    }
+    else
+    cout<<"a is < 5"<<endl;
+}
