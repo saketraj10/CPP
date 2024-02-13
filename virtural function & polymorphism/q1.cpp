24aea6d1d2341dc1a7827fc5d142bba70ae4909e
saketraj10 <110519922+saketraj10@users.noreply.github.com>
2024-02-13 20:01:55 +0530

    Add files via upload

---
+++ virtural function & polymorphism/q1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class base{
+    public:
+    void show(){
+        cout<<"base"<<endl;
+    }
+};
+class derv1: public base{
+    public: 
+    void show(){
+        cout<<"derv1"<<endl;
+    }
+};
+class derv2: public base{
+    public:
+    void show(){
+        cout<<"derv2"<<endl;
+    }
+};
+int main(){
+    derv1 dv1;
+    derv2 dv2;
+    //dv1.show();
+    //dv2.show();
+    base * ptr;
+    ptr = &dv1;
+    ptr->show();
+    ptr = &dv2;
+    ptr->show();
+    return 0;
+    
+}
